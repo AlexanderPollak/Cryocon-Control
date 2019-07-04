986e85ef159fceb830c209c2f5a95a9799c8bbac
alexanderpollak <Alexander.Pollak.87@gmail.com>
2019-07-04 17:17:24 +0100

    added functions to set the heater power level to MID and LOW

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     description='python package controls the Cryocon via the prologix GPIB to Ethernet adapter',
     license='MIT',
-    packages=['cryocon'],
+    packages=['cryocon, plx_gpib_ethernet'],
     author='Dr. Alexander Pollak',
     author_email='Alexander.Pollak.87@gmail.com',
     keywords=['Cryocon','GPIB'],
