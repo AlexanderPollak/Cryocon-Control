13836473468dad73d578dde1ae0421a075bf0050
alexanderpollak <Alexander.Pollak.87@gmail.com>
2019-07-04 17:44:41 +0100

    included the plx_gpib_ethernet class into the cryocon

--- cryocon/cryocon.py
+++ cryocon/cryocon.py
@@ -1,5 +1,63 @@
-import serial,time,numpy,re,time
-from plx_gpib_ethernet.plx_gpib_ethernet import *
+import serial,time,numpy,re,time,socket
+
+
+
+class PrologixGPIBEthernet:
+    PORT=1234
+
+    def __init__(self, host, timeout=1):
+        self.host = host
+        self.timeout = timeout
+        self.socket = socket.socket(socket.AF_INET,
+                                    socket.SOCK_STREAM,
+                                    socket.IPPROTO_TCP)
+        self.socket.settimeout(self.timeout)
+
+    def connect(self):
+        self.socket.connect((self.host, self.PORT))
+
+        self._setup()
+        return True
+
+    def close(self):
+        self.socket.close()
+        return True
+
+    def select(self, addr):
+        self._send('++addr %i' % int(addr))
+
+    def write(self, cmd):
+        self._send(cmd)
+
+    def read(self, num_bytes=1024):
+        self._send('++read eoi')
+        return self._recv(num_bytes)
+
+    def query(self, cmd, buffer_size=1024*1024):
+        self.write(cmd)
+        return self.read(buffer_size)
+
+    def _send(self, value):
+        encoded_value = ('%s\n' % value).encode('ascii')
+        self.socket.send(encoded_value)
+
+    def _recv(self, byte_num):
+        value = self.socket.recv(byte_num)
+        return value.decode('ascii')
+
+    def _setup(self):
+        # set device to CONTROLLER mode
+        self._send('++mode 1')
+
+        # disable read after write
+        self._send('++auto 0')
+
+        # set GPIB timeout
+        self._send('++read_tmo_ms %i' % int(self.timeout*1e3))
+
+        # do not require CR or LF appended to GPIB data
+        self._send('++eos 3')
+
 
 
 class control():
@@ -37,7 +95,7 @@ class control():
             return self.__adapter.query(cmd)
 
         except:
-            print"ERROR no communication possible, check if the connection has been opened with open()\r Or device did not send a reply!!!"
+            print"ERROR no communication possible, check if the connection has been opened with open(). Or device did not send a reply!!!"
 
     def disable(self):
         """ Disable the temperature stabilisation PID loop
--- plx_gpib_ethernet/__init__.py
+++
@@ -1,9 +0,0 @@
-from .plx_gpib_ethernet import PrologixGPIBEthernet
-from .plx_gpib_ethernet_device import PrologixGPIBEthernetDevice
-from .version import __version__
-
-__all__ = ['plx_gpib_ethernet',
-           'plx_gpib_ethernet_device',
-           'PrologixGPIBEthernet',
-           'PrologixGPIBEthernetDevice',
-           '__version__']
